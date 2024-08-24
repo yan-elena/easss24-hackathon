b5d8497bc897dddba47973fc6343d90c061202e5
Elena <elena.yan@studio.unibo.it>
2024-08-24 11:51:48 +0100

    init project

---
+++ src/agt/agent.asl
@@ -0,0 +1,9 @@
+!start.
+
+/* Plans */
+
++!start : true
+    <- .print("hello world.").
+
+
+{ include("$jacamo/templates/common-cartago.asl") }
---
+++ src/env/RESTArtifact.java
@@ -0,0 +1,60 @@
+import cartago.Artifact;
+import cartago.OPERATION;
+import cartago.OpFeedbackParam;
+import mams.web.HttpClient;
+import mams.web.HttpRequest;
+import mams.web.HttpResponse;
+import mams.web.RequestObject;
+import mams.web.WebResponse;
+import mams.web.WebUtils;
+
+public class RESTArtifact extends Artifact {
+    @OPERATION
+    void postRequest(String uri, String body, String mediaType, OpFeedbackParam<Integer> code, OpFeedbackParam<String> content) {
+        RequestObject requestObject = new RequestObject();
+        requestObject.method="POST";
+        requestObject.url=uri;
+        requestObject.content=body;
+        requestObject.type=mediaType;
+        try {
+            WebResponse response = WebUtils.sendRequest(requestObject);
+            code.set(response.getCode());
+            content.set(response.getContent());
+        } catch (Throwable th) {
+            th.printStackTrace();
+            System.exit(0);
+        }
+    }
+
+    private HttpClient client = HttpClient.newHttpClient();
+
+    @OPERATION
+    void execute(HttpRequest request, OpFeedbackParam<HttpResponse> response) {
+        response.set(client.execute(request));
+    }
+
+    @OPERATION
+    void getRequest(String uri, String mediaType, OpFeedbackParam<Integer> code, OpFeedbackParam<String> content) {
+        RequestObject requestObject = new RequestObject();
+        requestObject.method="GET";
+        requestObject.url=uri;
+        requestObject.content=null;
+        requestObject.type=mediaType;
+        WebResponse response = WebUtils.sendRequest(requestObject);
+        code.set(response.getCode());
+        content.set(response.getContent());
+    }
+
+    @OPERATION
+    void putRequest(String uri, String body, String mediaType, OpFeedbackParam<Integer> code, OpFeedbackParam<String> content) {
+        RequestObject requestObject = new RequestObject();
+        requestObject.method="PUT";
+        requestObject.url=uri;
+        requestObject.content=body;
+        requestObject.type=mediaType;
+        WebResponse response = WebUtils.sendRequest(requestObject);
+        code.set(response.getCode());
+        content.set(response.getContent());
+    }
+
+}
